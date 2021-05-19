2536308dd0450bd415973f4911265e58e06b2bc2
adam russell <adam38999@gmail.com>
2021-05-19 11:39:10 -0400

    rating funtionality

--- ratings/views.py
+++ ratings/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Frog
+from .forms import RatingForm
 
 def index(request):
+    if request.method == "POST":
+        rating = 0
+        if '1' in request.POST:
+            rating = 1
+        elif '2' in request.POST:
+            rating = 2
+        elif '3' in request.POST:
+            rating = 3
+        elif '4' in request.POST:
+            rating = 4
+        elif '5' in request.POST:
+            rating = 5
+        print(rating)
+        form = RatingForm(request.POST)
+        if form.is_valid():
+            url = form.cleaned_data['url']
+            print('form recieved')
+        return HttpResponseRedirect('/')
     allfrogs = Frog.objects.all()
     print(allfrogs[0].frog)
     return render(request, 'ratings/index.html', {'frog': allfrogs[0].frog})
