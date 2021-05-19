10e6e5ace125bbd0c613c14fddc236e96ee6517b
Emil Kovacev <emilkova@buffalo.edu>
2021-05-19 03:00:39 -0400

    made changes to responses to false commits; added -f --force tag to force add unsubmitted frogs in page

--- ratings/management/commands/populate.py
+++ ratings/management/commands/populate.py
@@ -5,10 +5,14 @@ import sys
 
 
 def get_frogs(page, page_size):
+    if page_size > 500:
+        raise CommandError
+
     response = requests.get('https://api.creativecommons.engineering/v1/images',
-    params={'q': 'frog', 'license': 'CC0,PDM', 'page': str(page), 'page_size': str(page_size)})
+    params={'q': 'frog', 'license': 'CC0,PDM', 'size': 'medium,large', 'page': str(page), 'page_size': str(page_size)})
     if response.status_code != 200:
         raise CommandError
+
     return response.json()
 
 
@@ -25,14 +29,25 @@ class Command(BaseCommand):
                             help='page number of frog query')
         parser.add_argument('page_size', type=int,
                             help='number of entries per page on query', default=50)
+        parser.add_argument('-f', '--force', action='store_true',
+                            help='saves all non-conflicting frogs to database')
     
     def handle(self, *args, **kwargs):
-        # page_number and page_size
-        frogs = get_frogs(kwargs['page_number'], kwargs['page_size'])
+        # command-line arguments
+        page_number = kwargs['page_number']
+        page_size = kwargs['page_size']
+        force = kwargs['force']
+
+        frogs = get_frogs(page_number, page_size)
+        i = 0
+
         for f in frogs['results']:
-            if Frog.objects.filter(title=f['title'], url=f['url']).exists():
-                raise RePopulationError(f"Invalid request to repopulate page {kwargs['page_number']}")
-            frog = Frog(title=f['title'], url=f['url'])
-            frog.save()
+            exists = Frog.objects.filter(title=f['title'], url=f['url']).exists()
+            if not force and exists:
+                raise RePopulationError(f"Page {kwargs['page_number']} is already populated")
+            elif not exists:
+                i += 1
+                frog = Frog(title=f['title'], url=f['url'])
+                frog.save()
  
-        self.stdout.write(self.style.SUCCESS(f"Successfully added {kwargs['page_size']} frogs"))
+        self.stdout.write(self.style.SUCCESS(f"Successfully added {i} frogs"))
